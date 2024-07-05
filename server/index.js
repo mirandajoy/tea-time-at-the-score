44d8500fcece6eddf9e594476165b3607afc3c8b
Charlotte <charlotteyuqiu@hotmail.com>
2024-07-04 23:09:55 -0400

    set up port 8080 and gets running

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
-import cors from 'cors';
-import express from 'express';
-import 'dotenv/config';
+import cors from "cors";
+import express from "express";
+import "dotenv/config";
 
 const app = express();
+
 const { PORT, CROSS_ORIGIN } = process.env;
 
 app.use(cors({ origin: CROSS_ORIGIN }));
 
-app.listen(PORT, () => {
-  console.log(`This app is listening on port ${PORT}`);
+app.listen(PORT || 8080, () => {
+  console.log(`This app is listening on port ${PORT || 8080}`);
 });

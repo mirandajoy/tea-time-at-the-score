2153ee5c1060978eb4858f27cda7f931c9c4520f
alisonroot <alisonlroot@gmail.com>
2024-07-05 13:15:58 -0600

    added links

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,12 +1,15 @@
 import "./Header.scss";
 import logo from "../../assets/logos/the-score-wordmark.svg";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header>
       <nav className="nav">
         <div className="nav__content">
-          <img src={logo} alt="The Score logo" className="nav__logo" />
+          <Link to="/">
+            <img src={logo} alt="The Score logo" className="nav__logo" />
+          </Link>
         </div>
       </nav>
     </header>

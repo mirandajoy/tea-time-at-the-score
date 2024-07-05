c27829e8ceacb9906c9906a156fb10b35e6cb6a7
kirandhatt <169182085+kirandhatt@users.noreply.github.com>
2024-07-05 10:27:53 -0700

    create basic structure for article

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,18 @@ import LiveVideo from "./components/LiveVideo/LiveVideo";
 import HomePage from "./pages/HomePage/HomePage";
 import StylesTemp from "./pages/StylesTemp/StylesTemp"; {/* REMOVE PRIOR TO DEPLOYMENT */}
 import PageLayout from "./components/PageLayout/PageLayout";
+import ArticlePage from "./pages/ArticlePage/ArticlePage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Header />
-        <LiveVideo />
         <PageLayout>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/:athleteId" element={<HomePage />} />
+            <Route path="/article" element={<ArticlePage />} />
             <Route path="/styles" element={<StylesTemp />} /> {/* REMOVE PRIOR TO DEPLOYMENT */}
           </Routes>
         </PageLayout>
